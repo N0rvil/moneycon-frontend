e8f5f90c75ead7495f58498db6c0754a9d646d29
Filip Duda <dudafilip80@seznam.cz>
2022-03-07 22:44:48 +0100

    fixing english mistakes

--- src/pages/Info/Info.js
+++ src/pages/Info/Info.js
@@ -14,9 +14,10 @@ const Info = () => {
             <div className='info__box'>
                 <h2 className='info__header'>1. Sing Up</h2>
                 <p className='info__text'>
-                    First thing you have to do is create your own account. You only need two things.
-                    Your e-mail address and some strong password. Please keep in mind the stronger your password is 
-                    the more secure is your account is. E-mail verification is not required. 
+                    The first thing you have to do is create your account. 
+                    You only need two things: your e-mail address and a strong password.
+                    Please keep in mind the stronger your password is, the more secure is your account is. 
+                    E-mail verification is not required. 
                 </p>
             </div>
             <img src='./images/signup.PNG' alt='info-img' className='info__image' />
@@ -26,11 +27,12 @@ const Info = () => {
             <div className='info__box'>
                 <h2 className='info__header'>2. Create categories</h2>
                 <p className='info__text'>
-                    In default setting you have just one category in income which is sallary.
-                    If you want to create records in spendings or records with different names of 
-                    categories you have to create them. After clic on button add in settings just put the name of the 
-                    category and select the color. In this color the category will show in chart. You can easily delete 
-                    them just by clicking on small delete button.
+                    In the default setting, you have just one category in income, which is salary. 
+                    If you want to create records in spendings or records with different categories, 
+                    you have to create them. After clicking on the button add in settings, 
+                    just put the category's name and select the color. In this color, 
+                    the category will show in the chart. You can easily delete them just by 
+                    clicking on the small delete button.
                 </p>
             </div>
         </div>
@@ -38,9 +40,11 @@ const Info = () => {
             <div className='info__box'>
                 <h2 className='info__header'>3. Create records</h2>
                 <p className='info__text'>
-                    After you create your categories you can go to the income or spendings page and 
-                    finally create your records. Just select category and type amount of money. You will 
-                    see records from last 30 days. You can easily edit them by clicking on a small pen icon.
+                    After creating your categories, 
+                    you can go to the income or spendings page and create your records: 
+                    just select category and type the amount of money. 
+                    You will see records from the last 30 days. 
+                    You can easily edit them by clicking on a small pen icon.
                 </p>
             </div>
             <img src='./images/records.PNG' alt='info-img' className='info__image' />
@@ -50,10 +54,11 @@ const Info = () => {
             <div className='info__box'>
                 <h2 className='info__header'>4. Settings</h2>
                 <p className='info__text'>
-                    In Settings you can easily change currency in wich you want application show your 
-                    records and balance. You have to click on set button if you want to set new currency.
-                    If you will need you can change your password by just type your old 
-                    password and two times your new and you are good to go.
+                    In Settings, you can easily change the application's 
+                    currency to show you your records and balance. 
+                    You have to click on the set button to set the new currency. 
+                    If you need to, you can change your password by just typing your 
+                    old password and then typing the new one twice, and you are good to go.
                 </p>
             </div>
         </div>
